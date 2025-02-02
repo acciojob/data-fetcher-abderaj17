bc7d1c18edc1b7b0fe6d5a2d5473b81376f1ea31
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-02-02 22:15:15 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
+  const [addData, setAddData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responce = await fetch("https://dummyjson.com/products");
+        const data = await responce.json();
+
+        setAddData(data.products);
+        setLoading(false);
+      } catch (err) {
+        console.log("Error fetching data:", err);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading.....</p>;
+  return(
     <div>
-        {/* Do not remove the main div */}
+      <h2>Products</h2>
+      <ul>
+        {addData.map((item)=>(
+          <li key={item.id}><h4>{item.title}</h4> -- <br/>
+          {item.description}  <br/>
+          {item.category} --
+          <strong>{item.price}</strong> <br/>
+          <h5>{item.discountPercentage}</h5> <br/>
+          
+          </li>
+        ))}
+      </ul>
     </div>
   )
-}
+};
 
-export default App
+export default App;
